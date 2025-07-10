f7eb784be582c1dee6f7a0691d4f64be5349bd6e
0027009445 <ye.wenchen@iwhalecloud.com>
2025-07-10 13:50:54 +0800

    edit the model

--- packages/core/src/config/models.ts
+++ packages/core/src/config/models.ts
@@ -4,6 +4,6 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-export const DEFAULT_GEMINI_MODEL = 'deepseek-ai/DeepSeek-V3';
+export const DEFAULT_GEMINI_MODEL = '';
 export const DEFAULT_GEMINI_FLASH_MODEL = 'deepseek-ai/DeepSeek-V3';
 export const DEFAULT_GEMINI_EMBEDDING_MODEL = 'Qwen/Qwen3-Embedding-8B';
